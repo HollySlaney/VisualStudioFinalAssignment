747eba34021772eb57e0b1b60162d2b4668cb582
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-09-05 10:14:47 -0600

    Add project files.

---
+++ VisualStudioFinalAssignment/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+// Image increase size onmouseover function
+function increase() {
+    document.getElementById("img").style.width = '500px';
+}
+
+// Image decrease size onmouseout function
+function decrease() {
+    document.getElementById("img").style.width = '300px';
+}
+ 
